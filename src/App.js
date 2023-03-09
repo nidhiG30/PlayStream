b0618f6eb3c4f74a2558fd99dda38af2cc6ecfb6
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-09 12:18:00 +0530

    Basic setup done to start development.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import './App.css'
+
 function App() {
   return (
-    <div><h1>Namaste React</h1></div>
+    <div>
+      <h1 className="text-3xl font-bold text-cyan-800">Namaste React</h1>
+    </div>
   );
 }
 

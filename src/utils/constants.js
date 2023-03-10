a9add7679f8137deee1863c6847e609c3571a664
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-10 16:57:50 +0530

    Fetched YouTube's API alongwith the personal API key.

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = 'AIzaSyBowGcpZ8QVOKMAqOZAX-xFSqApza4jS9M';
+
+export const YOUTUBE_VIDEOS_API =
+  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=' +
+  GOOGLE_API_KEY;

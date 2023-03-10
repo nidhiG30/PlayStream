a460e54c7a8589c38079579c1923f39ccb7a273d
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-10 16:11:42 +0530

    Adding buttons for most watched genre.

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Button from './Button'
+
 
 const ButtonList = () => {
   return (
-    <div>ButtonList</div>
+    <div className='flex'>
+      <Button name="All"/>
+      <Button name="Gaming"/>
+      <Button name="Songs"/>
+      <Button name="Soccer"/>
+      <Button name="Badminton"/>
+      <Button name="Cooking"/>
+      <Button name="Live"/>
+      <Button name="Mantras"/>
+      <Button name="News"/>
+    </div>
   )
 }
 

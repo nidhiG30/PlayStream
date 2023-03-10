ea68193cee1250f394990b36b1c1169be8b9bb02
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-10 18:07:45 +0530

    Create the state that will get data from the API call and on every update or state change reconciliation process will trigger.

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { YOUTUBE_VIDEOS_API } from '../utils/constants';
 
 const VideoContainer = () => {
-  return (
-    <div>VideoContainer</div>
-  )
+  const [videos, setVideos] = useState([]); // we need a state var to loop on the videos, and trigger reconciliation on every update
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    console.log(json.items);
+    setVideos(json.items)
+  }
+
+  return <div>VideoContainer</div>
 }
 
 export default VideoContainer

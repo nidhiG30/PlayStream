637732d7d07530c8619c890ad4e42a258bbf035b
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-10 16:17:46 +0530

    Instead of writing multiple Button componensts, we can map over the items and provide them in the Button component alongwith the props to get the values.

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import Button from './Button'
 
+const list = ["All", "Live", "Gaming", "Songs", "Soccer", "Badminton", "Cooking", "Mantras", "News", "Sadhguru", "Tourism", "Comedy", "Podcasts"];
+
+const btnItems = list.map(items => <Button name={items}/>)
 
 const ButtonList = () => {
   return (
     <div className='flex'>
-      <Button name="All"/>
-      <Button name="Gaming"/>
-      <Button name="Songs"/>
-      <Button name="Soccer"/>
-      <Button name="Badminton"/>
-      <Button name="Cooking"/>
-      <Button name="Live"/>
-      <Button name="Mantras"/>
-      <Button name="News"/>
+      {btnItems}
     </div>
   )
 }

f713ae8cef36efacbab9415b29dafae1621f4d50
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-11 00:21:17 +0530

    Provided links to the video cards and menu in the side bar.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   const isMenuOpen = useSelector(store => store.app.isMenuOpen);
 
-  // Early return 
+  // Early return
   if (!isMenuOpen) return null;
 
   return (
     <div className='p-5 shadow-lg w-48'>
       <ul>
-        <li>Home</li>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
         <li>Shorts</li>
         <li>Subscriptions</li>
         <li>Movies</li>
@@ -30,7 +33,7 @@ const Sidebar = () => {
         <li>Movies</li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;

c4eb1ae6c236f01b75501e27c08aaa16339271f4
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-10 20:10:16 +0530

    Modified API which can fetch max 50 videos.

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import { YOUTUBE_VIDEOS_API } from '../utils/constants';
+import VideoCard from './VideoCard';
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]); // we need a state var to loop on the videos, and trigger reconciliation on every update
@@ -11,11 +12,16 @@ const VideoContainer = () => {
   const getVideos = async () => {
     const data = await fetch(YOUTUBE_VIDEOS_API);
     const json = await data.json();
-    console.log(json.items);
-    setVideos(json.items)
-  }
+    setVideos(json.items);
+  };
 
-  return <div>VideoContainer</div>
-}
+  return (
+    <div className='flex flex-wrap'>
+      {videos.map(video => (
+        <VideoCard key={video.id} info={video} />
+      ))}
+    </div>
+  );
+};
 
-export default VideoContainer
+export default VideoContainer;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-const GOOGLE_API_KEY = 'AIzaSyBowGcpZ8QVOKMAqOZAX-xFSqApza4jS9M';
+const GOOGLE_API_KEY = 'AIzaSyBqTjXDOCsVtUQM7Z1WyOS6BaUZQxiRg7U';
 
 export const YOUTUBE_VIDEOS_API =
-  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=' +
+  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=' +
   GOOGLE_API_KEY;

7c99f3f8c71f806ae38a021c903ebdb496869d5c
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-11 00:16:10 +0530

    App comp will take all the paths, and paths that are mentioned in children will be rendered inside the outlet component which is present inside Body component.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import MainContainer from './MainContainer';
+import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
 const Body = () => {
   return (
     <div className='grid grid-flow-col'>
       <Sidebar />
-      <MainContainer />
+      <Outlet />
     </div>
   );
 };

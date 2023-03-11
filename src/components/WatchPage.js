7a3d8e22800601cd521619c8c4b4559619d0a981
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-11 12:43:56 +0530

    To get the watch video ID, used useParams() hook but it will return only empty object not the actual ID.

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { closeMenu } from '../utils/appSlice';
 
 const WatchPage = () => {
+  const params = useParams();
+  console.log(params); // as of now this will provide an empty object with no ID in it bcoz it is not /something, these are search params not normal params
+
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());

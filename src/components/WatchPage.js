62bc09dbedb2f098f2a29c048c2bf2d20fe97f10
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-11 18:06:54 +0530

    useSearchParams() this hook will actually get us the watch video ID, as it has a 'get' method which helps us return the ID in the URL with the special character mentioned into it. That's why replaced useParams() with useSearchParams().

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,19 +1,30 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { closeMenu } from '../utils/appSlice';
 
 const WatchPage = () => {
-  const params = useParams();
-  console.log(params); // as of now this will provide an empty object with no ID in it bcoz it is not /something, these are search params not normal params
-
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get('v'));
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
 
-  return <div>WatchPage</div>;
+  return (
+    <div className='px-5'>
+      <iframe
+        width='1200'
+        height='600'
+        src={'https://www.youtube.com/embed/' + searchParams.get('v')}
+        title='YouTube video player'
+        frameBorder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;

49e5ffbbecbbd1e7271cea59db92e899619bc0a2
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-16 20:10:23 +0530

    Modified 'constants' and 'store' files.

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import appSlice from "./appSlice";
-
+import { configureStore } from '@reduxjs/toolkit';
+import appSlice from './appSlice';
+import chatSlice from './chatSlice';
+import searchSlice from './searchSlice';
 
 const store = configureStore({
   reducer: {
-    app: appSlice
-  }
+    app: appSlice,
+    search: searchSlice,
+    chat: chatSlice,
+  },
 });
 
 export default store;
